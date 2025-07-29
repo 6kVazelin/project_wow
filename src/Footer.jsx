9b43125837470020d834062d34ed00407994be0a
Ko7aLia <Ko7aLia@yandex.ru>
2025-07-29 23:40:49 +0400

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,15 +1,17 @@
-﻿function footer() {
+function footer() {
     return (
         <>
-      <div className='flow-root'>
+      <footer className='flow-root'>
 
                 <pre className=' float-left text-sm text-stone-600'>Cлужба поддержки:
-                    <a href="@" className='text-white'>support@example.com</a>
+
+                    <a href="@" className='text-white'> support@example.com</a>
+
                 </pre>
 
              <a href="@" className='float-right text-sm text-white'>Пользовательское соглашение</a>
 
-      </div>
+      </footer>
         </>
   );
 }
